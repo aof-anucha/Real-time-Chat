6f8815423f6b2e7f244be64e07796a99c201bc6d
aof-anucha <anucha.wilailert@gmail.com>
2024-04-24 19:26:00 +0700

    edit comment

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.post('/chat', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('chat message', (msg) => {
-        // io.emit('chat message', msg);
-        // console.log('message: ' + msg);
       });
     socket.on('disconnect', () => {
       console.log('user disconnected');
